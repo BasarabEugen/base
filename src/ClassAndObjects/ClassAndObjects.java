e75ff9b5b2ada520b2140a6437783db6f361f772
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-07-16 16:41:09 +0300

    base

--- src/ClassAndObjects/ClassAndObjects.java
+++ src/ClassAndObjects/ClassAndObjects.java
@@ -3,4 +3,15 @@ package ClassAndObjects;/*
  */
 
 public class ClassAndObjects {
+    public static void main(String[] args) {
+        Person persona1 = new Person();
+        persona1.name = "Alex";
+        persona1.age = 30;
+        System.out.println("My name " + persona1.name + " i have " + persona1.age + " years");
+    }
+}
+
+class Person {
+    String name;
+    int age;
 }
